f578c4fd21ef68631314bfeaf91816be13cfa760
Kiripeps <47979082+Kiripeps@users.noreply.github.com>
2021-07-05 14:07:25 +0300

    Add files via upload

---
+++ Script0Main.py
@@ -0,0 +1,12 @@
+import Script1Download as sc1
+import Script2ChekBroken as sc2
+
+#pathSha = input("Введите путь к папке для сохранения контрольной суммы: ")
+#pathFile = input("Введите путь к папке для сохранения файлов: ")
+pathSha = "E:\FilesSha"
+pathFile = "E:\Files"
+
+sc1.ftpDownload('FbaSha', pathSha)
+sc1.ftpDownload('Fba', pathFile)
+
+sc2.chekSum(pathFile, pathSha)
---
+++ Script1Download.py
@@ -0,0 +1,39 @@
+def ftpDownload(workDir, path):
+    import ftplib
+    import os
+    
+    ftp = ftplib.FTP('v4rts.beget.tech')
+    ftp.login('v4rts_test','1234Bb!')
+    ftp.cwd(workDir)
+    filenames = ftp.nlst()
+    
+    if os.path.exists(path):
+        print("Указанная папка существует")
+    else:
+        print("указанная папка не существует")
+        os.mkdir(path)
+    
+    listDownloadFile = os.listdir(path)
+    if(listDownloadFile == []):
+        fileRecording(filenames[0], filenames, ftp, path)
+    else:
+        name = listDownloadFile[-1]
+        if(name == filenames[-1]):
+            print("Все значения скачаны")
+        else:
+            fileRecording(name, filenames, ftp, path)
+    ftp.quit()
+    
+def fileRecording(filename, filenames, ftp, path):
+    import ftplib
+    import os
+    for filename in filenames:
+        host_file = os.path.join(path, filename)
+        try:
+            with open(host_file, 'wb') as local_file:
+                ftp.retrbinary('RETR ' + filename, local_file.write)
+        except ftplib.error_perm:
+            pass
+    
+#ftpDownload('FbaSha', "E:\FilesSha")
+#ftpDownload('Fba', "E:\Files")
